d7631e8aca243796420a837983d5a3d89c6154db
Fhad Sheikh <fhad@websity.ca>
2016-07-09 20:47:16 -0400

    Moved index.html outside of APP folder

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt){
 
         wiredep: {
             dist: {
-                src: ['app/index.html']
+                src: ['index.html']
             }
         },
 
