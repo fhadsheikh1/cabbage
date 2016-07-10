e7f91241f98bb0b5cd73499cc1580949eaddcd24
Fhad Sheikh <fhad@websity.ca>
2016-07-09 20:11:37 -0400

    Added Angular to project. Set up new ng module with name cabbage and no dependencies

---
+++ client/app/app.module.js
@@ -0,0 +1,13 @@
+/* Set Angular Module for Cabbage */
+
+(function(){
+
+    'use strict';
+
+    // Define name for angular module and it's dependencies
+    angular
+        .module('cabbage', [
+
+        ])
+
+})();
